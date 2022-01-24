b425df5ce9e3e175512555ff86facc101e9bdda0
Vlad <vvllaadd1233345@gmail.com>
2022-01-24 14:27:43 +0200

    feat(event/owneraction,event-visitors,containers/containerownersaction): refactoring OwnersAction

--- src/components/event/EventVisitors/Event-visitors.js
+++ src/components/event/EventVisitors/Event-visitors.js
@@ -1,61 +1,59 @@
-import React, { Component } from 'react';
-import ParticipantGroup from '../ParticipantGroup/Participant-group';
-import OwnersActions from '../OwnersAction/Owners-action';
-import ApprovedUsersActions from '../ApprovedUsersAction/Approved-users-action';
-import PendingUsersActions from '../PendingUsersAction/Pending-users-action';
-import DeniedUsersActions from '../DeniedUsersAction/Denied-users-action';
+import React, { Component } from "react";
+import ParticipantGroup from "../ParticipantGroup/Participant-group";
+import ContainerOwnersActions from "../../../containers/ContainerOwnersAction";
+import ApprovedUsersActions from "../ApprovedUsersAction/Approved-users-action";
+import PendingUsersActions from "../PendingUsersAction/Pending-users-action";
+import DeniedUsersActions from "../DeniedUsersAction/Denied-users-action";
 
 class EventVisitors extends Component {
+  render() {
+    const { isMyPrivateEvent, visitors, admins, isMyEvent } = this.props;
 
-    render() {
-        const { isMyPrivateEvent, visitors, admins, isMyEvent } = this.props;
-
-        return (
-            <div>
-                <ParticipantGroup
-                    disabled={false}
-                    users={admins}
-                    label="Admin"
-                    renderUserActions={(user) => (<OwnersActions
-                                                        user={user}
-                                                        isMyEvent={isMyEvent}
-                                                  />)}
-                />
-                <ParticipantGroup
-                    disabled={visitors.approvedUsers.length == 0}
-                    users={visitors.approvedUsers}
-                    label="Visitors"
-                    renderUserActions={(user) => (<ApprovedUsersActions
-                                                        user={user}
-                                                        isMyEvent={isMyEvent}
-                                                        isMyPrivateEvent={isMyPrivateEvent}
-                                                  />)}
-                />
-                {isMyPrivateEvent &&
-                    <ParticipantGroup
-                        disabled={visitors.pendingUsers.length == 0}
-                        users={visitors.pendingUsers}
-                        label="Pending users"
-                        renderUserActions={(user) => (<PendingUsersActions
-                                                            user={user}
-                                                            isMyEvent={isMyEvent}
-                                                      />)}
-                    />
-                }
-                {isMyPrivateEvent &&
-                    <ParticipantGroup
-                    disabled={visitors.deniedUsers.length == 0}
-                    users={visitors.deniedUsers}
-                    label="Denied users"
-                    renderUserActions={(user) => (<DeniedUsersActions
-                                                        user={user}
-                                                        isMyEvent={isMyEvent}
-                                                  />)}
-                    />
-                }
-            </div>
-        )
-    }
+    return (
+      <div>
+        <ParticipantGroup
+          disabled={false}
+          users={admins}
+          label="Admin"
+          renderUserActions={(user) => (
+            <ContainerOwnersActions user={user} isMyEvent={isMyEvent} />
+          )}
+        />
+        <ParticipantGroup
+          disabled={visitors.approvedUsers.length == 0}
+          users={visitors.approvedUsers}
+          label="Visitors"
+          renderUserActions={(user) => (
+            <ApprovedUsersActions
+              user={user}
+              isMyEvent={isMyEvent}
+              isMyPrivateEvent={isMyPrivateEvent}
+            />
+          )}
+        />
+        {isMyPrivateEvent && (
+          <ParticipantGroup
+            disabled={visitors.pendingUsers.length == 0}
+            users={visitors.pendingUsers}
+            label="Pending users"
+            renderUserActions={(user) => (
+              <PendingUsersActions user={user} isMyEvent={isMyEvent} />
+            )}
+          />
+        )}
+        {isMyPrivateEvent && (
+          <ParticipantGroup
+            disabled={visitors.deniedUsers.length == 0}
+            users={visitors.deniedUsers}
+            label="Denied users"
+            renderUserActions={(user) => (
+              <DeniedUsersActions user={user} isMyEvent={isMyEvent} />
+            )}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default EventVisitors;
---
+++ src/components/event/OwnersAction/OwnersAction.js
@@ -0,0 +1,35 @@
+﻿import React from "react";
+import SimpleModal from "../SimpleModal/Simple-modal";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+
+export default function OwnersActions({
+  user,
+  isMyEvent,
+  currentUserId,
+  eventId,
+  deleteFromOwners,
+}) {
+  const handlerDeleteFromOwners = () => deleteFromOwners(user.id, eventId);
+
+  {
+    return (
+      isMyEvent &&
+      user.id !== currentUserId && (
+        <div>
+          <SimpleModal
+            action={handlerDeleteFromOwners}
+            data={`Are you sure, that you wanna delete ${user.username} from owners?`}
+            button={
+              <Tooltip title="Delete from owners">
+                <IconButton aria-label="delete">
+                  <i className="far fa-trash-alt" />
+                </IconButton>
+              </Tooltip>
+            }
+          />
+        </div>
+      )
+    );
+  }
+}
