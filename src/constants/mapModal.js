074921b60247a18f4f5106e6143c571687d103ac
Marina20V <meri20vit@gmail.com>
2022-01-24 13:24:47 +0200

    Refactor code in MapModal component

---
+++ src/constants/mapModal.js
@@ -0,0 +1,14 @@
+const constants = {
+  FILTER_BY_LOCATION: "Filter by location",
+  RADIUS_IS: "Radius is ",
+  KM: "km",
+  CURRENT_MAP_POSITION: "Current position on the Map is:",
+  LATITUDE: "latitude:",
+  LONGITUDE: "longitude:",
+  LOCATION: "Location",
+  CHOOSE_MAP_POSITION: "Choose position on the Map!",
+  CANCEL: "Cancel",
+  APPLY: "Apply",
+};
+
+export default constants;
