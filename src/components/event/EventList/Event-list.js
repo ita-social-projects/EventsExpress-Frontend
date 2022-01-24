cdc28afc2b7143e7682493e27ea3c46189e15d4f
Depesi <anton.tsar02@gmail.com>
2022-01-24 17:30:34 +0200

    refactor: change Event-list.js, added imports and change useEffect

--- src/components/event/EventList/Event-list.js
+++ src/components/event/EventList/Event-list.js
@@ -3,12 +3,17 @@ import RenderList from '../RenderList/RenderList';
 import EventCard from '../EventItem/Event-item';
 import { parse as queryStringParse } from 'query-string';
 import filterHelper from '../../helpers/filterHelper';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import eventStatusEnum from '../../../constants/eventStatusEnum';
+import { reset_events, updateEventsFilters } from '../../../actions/event/event-list-action';
+import { change_event_status } from '../../../actions/event/event-item-view-action';
 
 const EventList = ({totalPages, history, current_user, onBlock, onUnBlock, ...props }) => {
 	useEffect(() => {
-		totalPages > 1 && history.location.search == ""
-		history.push(history.location.pathname + `?page=1`);
-	}, [])
+		(totalPages > 1 && history.location.search == "") &&
+			history.push(history.location.pathname + `?page=1`)
+	}, [totalPages, history.location.search])
 
 	handlePageChange = page => {
         const queryStringToObject = queryStringParse(history.location.search);
