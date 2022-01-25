ab71d3ac9118306155cf8fb1ca2ee251f9842950
Marina20V <meri20vit@gmail.com>
2022-01-26 01:05:43 +0200

    refactor: refactor eslint issues

--- src/components/event/EventList/Event-list.js
+++ src/components/event/EventList/Event-list.js
@@ -1,46 +1,73 @@
-import React from "react";
+/* eslint-disable no-unused-expressions */
+import React, { useEffect } from "react";
 import { parse as queryStringParse } from "query-string";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+
+import PropTypes from "prop-types";
+
 import RenderList from "../RenderList/RenderList";
 import EventCard from "../EventItem/Event-item";
 import filterHelper from "../../helpers/filterHelper";
 import eventStatusEnum from "../../../constants/eventStatusEnum";
-import {
-  reset_events,
-  updateEventsFilters,
-} from "../../../actions/event/event-list-action";
-import { change_event_status } from "../../../actions/event/event-item-view-action";
+// eslint-disable-next-line import/named
+import { resetEvents, updateEventsFilters } from "../../../actions/event/event-list-action";
+// eslint-disable-next-line import/named
+import { changedEventStatus } from "../../../actions/event/event-item-view-action";
+
 
 const EventList = ({
   totalPages,
   history,
-  current_user,
+  currentUser,
   onBlock,
   onUnBlock,
   ...props
 }) => {
+
+  EventList.propTypes = {
+    totalPages: PropTypes.number,
+    history: PropTypes.array,
+    currentUser: PropTypes.object,
+    onBlock: PropTypes.func,
+    onUnBlock: PropTypes.func
+  };
+  
+  EventList.defaultProps = {
+    totalPages: null,
+    history: [],
+    currentUser: {},
+    onBlock: () => {},
+    onUnBlock: () => {},
+  };
+
   useEffect(() => {
+    return(
     totalPages > 1 &&
-      history.location.search == "" &&
-      history.push(`${history.location.pathname}?page=1`);
+      history.location.search === "" &&
+      history.push(`${history.location.pathname}?page=1`)
+    );
   }, [totalPages, history.location.search]);
 
-  handlePageChange = page => {
+  const handlePageChange = page => {
     const queryStringToObject = queryStringParse(history.location.search);
-    history.location.search == ""
-      ? history.push(`${history.location.pathname}?page=${page}`)
-      : (queryStringToObject.page = page);
+    if(history.location.search === "") {
+      history.push(`${history.location.pathname}?page=${page}`)
+    }else {
+      queryStringToObject.page = page;
+    };
+
+    // eslint-disable-next-line no-param-reassign
     history.location.search =
       filterHelper.getQueryStringByFilter(queryStringToObject);
     history.push(history.location.pathname + history.location.search);
   };
 
-  renderSingleItem = item => (
+  const renderSingleItem = item => (
     <EventCard
       key={item.id + item.Active}
       item={item}
-      current_user={current_user}
+      current_user={currentUser}
       onBlock={onBlock}
       onUnBlock={onUnBlock}
     />
@@ -56,12 +83,12 @@ const EventList = ({
 
 const mapDispatchToProps = dispatch => {
   return {
-    reset_events: () => dispatch(reset_events()),
+    resetEvents: () => dispatch(resetEvents()),
     updateEventsFilters: filter => dispatch(updateEventsFilters(filter)),
     onBlock: (eventId, reason) =>
-      dispatch(change_event_status(eventId, reason, eventStatusEnum.Blocked)),
+      dispatch(changedEventStatus(eventId, reason, eventStatusEnum.Blocked)),
     onUnBlock: (eventId, reason) =>
-      dispatch(change_event_status(eventId, reason, eventStatusEnum.Active)),
+      dispatch(changedEventStatus(eventId, reason, eventStatusEnum.Active)),
   };
 };
 
