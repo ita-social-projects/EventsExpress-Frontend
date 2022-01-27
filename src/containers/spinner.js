70a264225105439583b1d2e3901044d69387addf
Stanislav-Vyhnan <stas.vignan@gmail.com>
2022-01-27 22:52:55 +0200

    fix(shared | spinner.js | Stepper.js | ThreeStateCheckbox.js): fixed es-lint errors

--- src/containers/spinner.js
+++ src/containers/spinner.js
@@ -1,17 +1,24 @@
-﻿import React, { Component } from "react";
+﻿import React from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import Spinner from "../components/spinner";
 
-class SpinnerWrapper extends Component {
-  render() {
-    const { counter, showContent } = this.props;
-
-    return counter > 0 || !showContent ? <Spinner /> : this.props.children;
-  }
-}
+const SpinnerWrapper = ({ counter, showContent, children }) =>
+  counter > 0 || !showContent ? <Spinner /> : children;
 
 const mapStateToProps = state => ({
   counter: state.requestCount.counter,
 });
 
+SpinnerWrapper.defaultProps = {
+  counter: null,
+  children: {},
+  showContent: false,
+};
+
+SpinnerWrapper.propTypes = {
+  counter: PropTypes.number,
+  children: PropTypes.object,
+  showContent: PropTypes.bool,
+};
 export default connect(mapStateToProps)(SpinnerWrapper);
