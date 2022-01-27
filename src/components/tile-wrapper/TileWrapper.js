6d1c44f76e263d88ca8d0222065c5271fc8b4207
Stanislav-Vyhnan <stas.vignan@gmail.com>
2022-01-28 00:45:37 +0200

    fix(TileWrapper.js | Tile.js): fixed es-lint errors

--- src/components/tile-wrapper/TileWrapper.js
+++ src/components/tile-wrapper/TileWrapper.js
@@ -1,22 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Tile from "../tile/Tile";
 import "./TileWrapper.css";
 
-const TileWrapper = props => {
-  const handleTileToggleAction = () => {
-    props.handleTileToggleAction(props.index);
+const TileWrapper = ({ index, value, handleTileToggleAction }) => {
+  const handleTile = () => {
+    handleTileToggleAction(index);
   };
 
   return (
     <div className="tile-wrapper">
       <Tile
-        groupId={props.value.group.id}
-        categories={props.value.categories}
-        handleTileToggleAction={handleTileToggleAction}
+        groupId={value.group.id}
+        categories={value.categories}
+        handleTileToggleAction={handleTile}
       />
-      <p className="tile-label">{props.value.group.title}</p>
+      <p className="tile-label">{value.group.title}</p>
     </div>
   );
+};
+
+TileWrapper.defaultProps = {
+  index: null,
+  handleTileToggleAction: () => {},
+  value: {},
+};
+
+TileWrapper.propTypes = {
+  index: PropTypes.number,
+  handleTileToggleAction: PropTypes.func,
+  value: PropTypes.object,
 };
 
 export default TileWrapper;
