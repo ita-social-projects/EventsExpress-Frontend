b6efba07e8264191f25f9d176abea8677e315e63
Marina20V <meri20vit@gmail.com>
2022-02-01 10:21:28 +0200

    refactor: refactor render period function

--- src/components/eventSchedule/render-period.js
+++ src/components/eventSchedule/render-period.js
@@ -1,27 +1,19 @@
 import "moment-timezone";
 import { enumPeriodicity } from "../../constants/PeriodicityConstants";
 
-function renderPeriod(periodicity, frequency) {
-  switch (periodicity) {
-    case enumPeriodicity.Day && frequency > 1:
-      return `in ${frequency} days`;
-    case enumPeriodicity.Day && frequency === 1:
-      return "in a day";
-    case enumPeriodicity.Week && frequency > 1:
-      return `in ${frequency} weeks`;
-    case enumPeriodicity.Week && frequency === 1:
-      return "in a week";
-    case enumPeriodicity.Month && frequency > 1:
-      return `in ${frequency} months`;
-    case enumPeriodicity.Month && frequency === 1:
-      return "in a month";
-    case enumPeriodicity.Year && frequency > 1:
-      return `in ${frequency} years`;
-    case enumPeriodicity.Year && frequency === 1:
-      return "in a year";
-    default:
-      return null;
-  }
-}
+const renderPeriod = (periodicity, frequency) => {
+  const periods = ["Day", "Week", "Month", "Year"];
+
+  periods.forEach(period => {
+    let value = "";
+    if (periodicity === enumPeriodicity.period && frequency > 1) {
+      value = `in ${frequency} ${period.toLowerCase}s`;
+    }
+    if (periodicity === enumPeriodicity.period && frequency === 1) {
+      value = `in a ${period.toLowerCase}`;
+    }
+    return value;
+  });
+};
 
 export default renderPeriod;
