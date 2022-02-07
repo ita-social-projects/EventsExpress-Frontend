cd5d57e33d0dd4f6bd02291e395281df0b718d00
SvitlanaYaroslavicka1 <77430580+SvitlanaYaroslavicka1@users.noreply.github.com>
2022-02-07 14:16:42 +0200

    refactor Event-item.js

--- src/components/event/EventItem/Event-item.js
+++ src/components/event/EventItem/Event-item.js
@@ -1,4 +1,4 @@
-﻿import React, { Component } from "react";
+﻿import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "moment-timezone";
 import Card from "@material-ui/core/Card";
@@ -24,166 +24,153 @@ import eventDefaultImage from "../../../constants/eventDefaultImage";
 const useStyles = useStyle;
 const photoService = new PhotoService();
 
-export default class EventCard extends Component {
-  constructor(props) {
-    super(props);
+const EventCard = props => {
+  const { item, onBlock, onUnBlock } = props;
+  const [eventImage, setEventImage] = useState(eventDefaultImage);
 
-    this.state = {
-      eventImage: eventDefaultImage,
+  useEffect(() => {
+    photoService.getPreviewEventPhoto(item.id).then(eventPreviewImage => {
+      if (eventPreviewImage) {
+        setEventImage(URL.createObjectURL(eventPreviewImage));
+      }
+    });
+    return () => {
+      URL.revokeObjectURL(eventImage);
     };
-  }
+  });
 
-  componentDidMount() {
-    photoService
-      .getPreviewEventPhoto(this.props.item.id)
-      .then(eventPreviewImage => {
-        if (eventPreviewImage != null) {
-          this.setState({ eventImage: URL.createObjectURL(eventPreviewImage) });
-        }
-      });
-  }
-
-  componentWillUnmount() {
-    URL.revokeObjectURL(this.state.eventImage);
-  }
-
-  renderCategories = arr => {
+  const renderCategories = arr => {
     return arr.map(x => <span key={x.id}>#{x.name}</span>);
   };
+  const classes = useStyles;
+  const {
+    id,
+    title,
+    dateFrom,
+    description,
+    isPublic,
+    isOnlyForAdults,
+    maxParticipants,
+    eventStatus,
+    categories,
+    countVisitor,
+    organizers: owners,
+    members,
+  } = item;
+  const INT32_MAX_VALUE = null;
+  const categoriesNotDisplayed = categories.length - 2;
+  const restCategories = ` ... ${categoriesNotDisplayed} more`;
+  const displayedCategories = renderCategories(categories.slice(0, 2));
 
-  render() {
-    const classes = useStyles;
-    const {
-      id,
-      title,
-      dateFrom,
-      description,
-      isPublic,
-      isOnlyForAdults,
-      maxParticipants,
-      eventStatus,
-      categories,
-      countVisitor,
-      organizers: owners, // Did this for correction data of owners,
-      members,
-    } = this.props.item;
-    const INT32_MAX_VALUE = null;
-    const categoriesNotDisplayed = categories.length - 2;
-    const restCategories = ` ... ${categoriesNotDisplayed} more`;
-    const displayedCategories = this.renderCategories(categories.slice(0, 2));
-
-    return (
-      <div className="col-12 col-sm-8 col-md-6 col-xl-4 mt-3">
-        <Card
-          className={classes.cardCanceled}
-          style={{
-            backgroundColor:
-              eventStatus === eventStatusEnum.Blocked ? "gold" : "",
-            opacity: eventStatus === eventStatusEnum.Canceled ? 0.5 : 1,
-          }}
-        >
-          <EventHeader
-            members={members}
-            countVisitor={countVisitor}
-            owners={owners}
-            dateFrom={dateFrom}
-            title={title}
-          />
-          <CardMedia className={classes.media} title={title}>
-            <Link to={`/event/${id}/1`} id="LinkToEvent">
-              <img
-                src={this.state.eventImage}
-                id={`eventPreviewPhotoImg${id}`}
-                alt="Event"
-                className="w-100"
-              />
-            </Link>
-          </CardMedia>
-          {maxParticipants < INT32_MAX_VALUE && (
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {countVisitor}/{maxParticipants} Participants
-              </Typography>
-            </CardContent>
-          )}
+  return (
+    <div className="col-12 col-sm-8 col-md-6 col-xl-4 mt-3">
+      <Card
+        className={classes.cardCanceled}
+        style={{
+          backgroundColor:
+            eventStatus === eventStatusEnum.Blocked ? "gold" : "",
+          opacity: eventStatus === eventStatusEnum.Canceled ? 0.5 : 1,
+        }}
+      >
+        <EventHeader
+          members={members}
+          countVisitor={countVisitor}
+          owners={owners}
+          dateFrom={dateFrom}
+          title={title}
+        />
+        <CardMedia className={classes.media} title={title}>
+          <Link to={`/event/${id}/1`} id="LinkToEvent">
+            <img
+              src={eventImage}
+              id={`eventPreviewPhotoImg${id}`}
+              alt="Event"
+              className="w-100"
+            />
+          </Link>
+        </CardMedia>
+        {maxParticipants < INT32_MAX_VALUE && (
           <CardContent>
-            {description && (
-              <Tooltip
-                title={
-                  description.substr(0, 570) +
-                  (description.length > 570 ? "..." : "")
-                }
-                classes={{ tooltip: "description-tooltip" }}
-              >
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  className="description"
-                  component="p"
-                >
-                  {description.substr(0, 128)}
-                </Typography>
-              </Tooltip>
-            )}
+            <Typography variant="body2" color="textSecondary" component="p">
+              {countVisitor}/{maxParticipants} Participants
+            </Typography>
           </CardContent>
-          <CardActions disableSpacing>
-            <div className="w-100">
-              {this.props.item.location && (
-                <DisplayLocation location={this.props.item.location} />
+        )}
+        <CardContent>
+          {description && (
+            <Tooltip
+              title={
+                description.substr(0, 570) +
+                (description.length > 570 ? "..." : "")
+              }
+              classes={{ tooltip: "description-tooltip" }}
+            >
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                className="description"
+                component="p"
+              >
+                {description.substr(0, 128)}
+              </Typography>
+            </Tooltip>
+          )}
+        </CardContent>
+        <CardActions disableSpacing>
+          <div className="w-100">
+            {item.location && <DisplayLocation location={item.location} />}
+            <br />
+            <div className="float-left">
+              {categories.length <= 2 ? (
+                displayedCategories
+              ) : (
+                <>
+                  {displayedCategories},
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="span"
+                  >
+                    {restCategories}
+                  </Typography>
+                  ,
+                </>
               )}
-              <br />
-              <div className="float-left">
-                {categories.length <= 2 ? (
-                  displayedCategories
-                ) : (
-                  <>
-                    {displayedCategories},
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="span"
-                    >
-                      {restCategories}
-                    </Typography>
-                    ,
-                  </>
-                )}
-              </div>
-              <div className="d-flex flex-row align-items-center justify-content-center float-right">
-                {isOnlyForAdults && (
-                  <Tooltip title="Only for adults">
-                    <span className="age-icon">&#128286;</span>
-                  </Tooltip>
-                )}
-                {!isPublic && (
-                  <Tooltip title="Private event">
-                    <IconButton>
-                      <Badge color="primary">
-                        <i className="fa fa-key" />
-                      </Badge>
-                    </IconButton>
-                  </Tooltip>
-                )}
-                <AuthComponent rolesMatch={Roles.Admin}>
-                  <EventActiveStatus
-                    key={this.props.item.id + this.props.item.eventStatus}
-                    eventStatus={this.props.item.eventStatus}
-                    eventId={this.props.item.id}
-                    onBlock={this.props.onBlock}
-                    onUnBlock={this.props.onUnBlock}
-                  />
-                </AuthComponent>
-                <SocialShareMenu
-                  href={`${window.location.protocol}//${window.location.host}/event/${id}/1`}
-                />
-              </div>
             </div>
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-}
+            <div className="d-flex flex-row align-items-center justify-content-center float-right">
+              {isOnlyForAdults && (
+                <Tooltip title="Only for adults">
+                  <span className="age-icon">&#128286;</span>
+                </Tooltip>
+              )}
+              {!isPublic && (
+                <Tooltip title="Private event">
+                  <IconButton>
+                    <Badge color="primary">
+                      <i className="fa fa-key" />
+                    </Badge>
+                  </IconButton>
+                </Tooltip>
+              )}
+              <AuthComponent rolesMatch={Roles.Admin}>
+                <EventActiveStatus
+                  key={item.id + item.eventStatus}
+                  eventStatus={item.eventStatus}
+                  eventId={item.id}
+                  onBlock={onBlock}
+                  onUnBlock={onUnBlock}
+                />
+              </AuthComponent>
+              <SocialShareMenu
+                href={`${window.location.protocol}//${window.location.host}/event/${id}/1`}
+              />
+            </div>
+          </div>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
 EventCard.propTypes = {
   item: propTypes.object,
@@ -196,3 +183,5 @@ EventCard.defaultProps = {
   onBlock: () => {},
   onUnBlock: () => {},
 };
+
+export default EventCard;
