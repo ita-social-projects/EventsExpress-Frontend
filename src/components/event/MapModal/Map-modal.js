464f9ed211d1c361b2e788be353f1aced9e26a49
Marina20V <meri20vit@gmail.com>
2022-02-07 17:52:24 +0200

    style(mapmodal): delete comment

--- src/components/event/MapModal/Map-modal.js
+++ src/components/event/MapModal/Map-modal.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
