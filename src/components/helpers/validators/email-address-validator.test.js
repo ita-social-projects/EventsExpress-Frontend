f25739a05f1b512b450e1b8eb501a64b927efadc
Lesia Mandziuk <92401462+LesiaM-T@users.noreply.github.com>
2022-03-17 11:27:19 +0200

    feat(email-address-validator.test): unit test for email-adress-validator

--- src/components/helpers/validators/email-address-validator.test.js
+++ src/components/helpers/validators/email-address-validator.test.js
@@ -1,12 +1,24 @@
 import isValidEmail from "./email-address-validator";
 
 describe("test validation email helper", () => {
+  const error = "Invalid email address";
+
   it("test with correct email", () => {
     const result = isValidEmail("ttttttt@uuu.uu");
-    expect(result).toBe(true);
+    expect(result).toStrictEqual({});
   });
+
   it("test with incorrect email", () => {
     const result = isValidEmail("tttttttuuu.uu");
-    expect(result).toBe(false);
+    expect(result).toStrictEqual({ email: error });
+  });
+
+  it("test with incorrect email", () => {
+    const result = isValidEmail("ttttt@ttuu");
+    expect(result).toStrictEqual({ email: error });
+  });
+  it("test with empty email string", () => {
+    const result = isValidEmail(" ");
+    expect(result).toStrictEqual({ email: error });
   });
 });
