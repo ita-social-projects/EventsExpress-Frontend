28fc31cb2781548e1ee5e66f41a0eb55ccc32c0a
Marina20V <meri20vit@gmail.com>
2022-03-28 01:05:38 +0300

    test(users-data): fix test

--- src/reducers/events-filter/users-data.test.js
+++ src/reducers/events-filter/users-data.test.js
@@ -3,13 +3,13 @@ import usersDataReducer from "./users-data";
 
 describe("Users Data Reducer", () => {
   it("Should return default state", () => {
-    const newState = usersDataReducer(undefined, {});
+    const newState = usersDataReducer([], {});
     expect(newState).toEqual([]);
   });
 
   it("Should return new state if receiving type", () => {
     const users = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    const newState = usersDataReducer({
+    const newState = usersDataReducer([], {
       type: SET_USERS,
       payload: users,
     });
