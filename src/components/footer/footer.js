39f1080488bc6d3549a0a308714a58c98bed0f51
SvitlanaYaroslavicka1 <77430580+SvitlanaYaroslavicka1@users.noreply.github.com>
2022-03-29 10:52:00 +0300

    Redesign footer (#88)
    * Merge branch 'development' into redesign-events-cards-matrix-view
    
    * redesign footer
    
    * delete margin-top and add tag form
    
    * style(footer.js, footer.js): create constants, delete redundant div
    
    * chore(delete extra spaces): elete extra spaces

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,37 +1,62 @@
 import React from "react";
-import "./footer.css";
 import { Link } from "react-router-dom";
+import Logo from "../shared/Logo/logo";
+import "./footer.scss";
+import constants from "../../constants/Footer";
+
+const { SUBSCRIBE_US, SUBSCRIBE, CONTACT_US, PHONE_NUMBER } = constants;
+
+const links = [
+  { id: 0, name: "Home", path: "/home" },
+  { id: 1, name: "Search Event", path: "/events" },
+  { id: 2, name: "Create Event", path: "" },
+  { id: 3, name: "Terms", path: "/terms" },
+  { id: 4, name: "About", path: "/about" },
+];
+
+const socialLinks = [
+  { id: 0, icon: "fab fa-facebook-f", link: "https://uk-ua.facebook.com" },
+  { id: 1, icon: "fab fa-instagram", link: "https://www.instagram.com" },
+  { id: 2, icon: "fab fa-youtube", link: "https://www.instagram.com" },
+];
 
 const Footer = () => {
-  const links = [
-    { id: 0, name: "Privacy", path: "/privacy" },
-    { id: 1, name: "Terms", path: "/terms" },
-    { id: 2, name: "About", path: "/about" },
-    { id: 3, name: "Contact us", path: "/contactAdmin" },
-  ];
-
-  const socialLinks = [
-    { id: 0, icon: "fab fa-facebook-f", link: "https://uk-ua.facebook.com" },
-    { id: 1, icon: "fab fa-instagram", link: "https://www.instagram.com" },
-    { id: 2, icon: "fab fa-youtube", link: "https://www.instagram.com" },
-  ];
-
   return (
     <footer className="custom-footer">
+      <div className="custom-footer__logo">
+        <Logo />
+      </div>
       <div className="links-to-pages">
         {links.map(link => (
           <Link key={link.id} to={link.path} className="nav-link link">
-            <i className="link-circle fas fa-circle" />
+            <i className="link-circle fas fa-circle link-text" />
             {link.name}
           </Link>
         ))}
       </div>
+      <div className="custom-footer__subscribe">
+        <h4>{SUBSCRIBE_US}</h4>
+        <form>
+          <input
+            type="email"
+            value="Paste your email"
+            className="custom-footer__subscribe i"
+          ></input>
+          <button type="submit" className="btn-dark-theme">
+            {SUBSCRIBE}
+          </button>
+        </form>
+      </div>
       <div className="social-links">
-        {socialLinks.map(link => (
-          <Link key={link.id} to={link.link} className="nav-link social-link">
-            <i className={link.icon} />
-          </Link>
-        ))}
+        <h4>{CONTACT_US}</h4>
+        <p>{PHONE_NUMBER}</p>
+        <div className="social-links__icons">
+          {socialLinks.map(link => (
+            <Link key={link.id} to={link.link} className="nav-link social-link">
+              <i className={link.icon} />
+            </Link>
+          ))}
+        </div>
       </div>
     </footer>
   );
