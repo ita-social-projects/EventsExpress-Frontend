56ffec2e8d217202fe060095d198b5c4c235471a
SvitlanaYaroslavicka1 <77430580+SvitlanaYaroslavicka1@users.noreply.github.com>
2022-04-08 17:53:00 +0300

    feat:refactoring and created responsive design for footer (#98)
    * refactored and created responsive design
    
    * style(created own file for mixins): created own file for mixins
    
    * refactor(fixed mistakes): fixed mistakes
    
    changed mapper, added constants, delete em, rename div
    
    * fix(footer.js footer.js): rename path
    
    * refactor(footer.js  footer. scss): delete coment, rename mapper
    
    * refactor(footer-mapper.js footer.js footerconstants.js): move mappers, group constants
    
    group some of these constants into few categories, move mapper outside of component
    
    * refactor(footer-maper.js): make  constant varibles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,62 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../shared/Logo/logo";
+import { NavLink } from "react-router-dom";
+import { IoMdSend } from "react-icons/io";
+import LogoWhite from "../../assets/icons/LogoWhite.png";
 import "./footer.scss";
-import constants from "../../constants/Footer";
+import { FOOTER_LABELS } from "../../constants/footerConstants";
+import { SOCIAL_NAV_LINKS, NAV_LINKS } from "./footer-mapper";
 
-const { SUBSCRIBE_US, SUBSCRIBE, CONTACT_US, PHONE_NUMBER } = constants;
-
-const links = [
-  { id: 0, name: "Home", path: "/home" },
-  { id: 1, name: "Search Event", path: "/events" },
-  { id: 2, name: "Create Event", path: "" },
-  { id: 3, name: "Terms", path: "/terms" },
-  { id: 4, name: "About", path: "/about" },
-];
-
-const socialLinks = [
-  { id: 0, icon: "fab fa-facebook-f", link: "https://uk-ua.facebook.com" },
-  { id: 1, icon: "fab fa-instagram", link: "https://www.instagram.com" },
-  { id: 2, icon: "fab fa-youtube", link: "https://www.instagram.com" },
-];
+const { SUBSCRIBE_US, CONTACT_US, PHONE_NUMBER, PASTE_YOUR_EMAIL, HOME } =
+  FOOTER_LABELS;
 
 const Footer = () => {
   return (
-    <footer className="custom-footer">
-      <div className="custom-footer__logo">
-        <Logo />
+    <footer className="footer__container">
+      <div className="footer__links_container">
+        <NavLink to={HOME}>
+          <img src={LogoWhite} alt="Logo" className="footer__logo" />
+        </NavLink>
+        <div className="footer__links">{NAV_LINKS}</div>
       </div>
-      <div className="links-to-pages">
-        {links.map(link => (
-          <Link key={link.id} to={link.path} className="nav-link link">
-            <i className="link-circle fas fa-circle link-text" />
-            {link.name}
-          </Link>
-        ))}
-      </div>
-      <div className="custom-footer__subscribe">
+      <div className="footer__subscribe">
         <h4>{SUBSCRIBE_US}</h4>
-        <form>
+        <form className="footer__form">
           <input
             type="email"
-            value="Paste your email"
-            className="custom-footer__subscribe i"
-          ></input>
-          <button type="submit" className="btn-dark-theme">
-            {SUBSCRIBE}
-          </button>
+            placeholder={PASTE_YOUR_EMAIL}
+            className="footer__input"
+          />
+          <IoMdSend className="footer__subscribe__icon" />
         </form>
       </div>
-      <div className="social-links">
+      <div className="footer__social_links">
         <h4>{CONTACT_US}</h4>
         <p>{PHONE_NUMBER}</p>
-        <div className="social-links__icons">
-          {socialLinks.map(link => (
-            <Link key={link.id} to={link.link} className="nav-link social-link">
-              <i className={link.icon} />
-            </Link>
-          ))}
-        </div>
+        <div className="footer__social_links_icons">{SOCIAL_NAV_LINKS}</div>
       </div>
     </footer>
   );
