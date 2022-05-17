4831f2c42c8149e23d13298faa3216fe4aeb57da
Melya <melenko55@gmail.com>
2022-05-17 19:16:28 +0300

    recreate banner

--- src/components/landing/HeadArticle/HeadArticle.js
+++ src/components/landing/HeadArticle/HeadArticle.js
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 import image1 from "../landing-images/1.jpg";
 import image2 from "../landing-images/2.jpg";
 import image3 from "../landing-images/3.jpg";
@@ -7,8 +10,9 @@ import image5 from "../landing-images/5.jpg";
 import image6 from "../landing-images/6.jpg";
 import image7 from "../landing-images/7.jpg";
 import image8 from "../landing-images/8.jpg";
-// import HeadArticleButtons from "../HeadArticleButtons/HeadArticleButtons";
-import HeadArticleSearch from "../HeadArticleSearch/HeadArticleSearch";
+
+import "./HeadArticle.scss";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const imagesPreload = [
   image1,
@@ -21,40 +25,37 @@ const imagesPreload = [
   image8,
 ];
 
-class HeadArticle extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentImage: image1,
-      currentImageCounter: 0,
-    };
-  }
+const HeadArticle = () => {
+  const [currentImage, setCurrentImage] = useState(0);
 
-  async componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState((prevState) => ({
-        currentImageCounter:
-          (prevState.currentImageCounter + 1) % imagesPreload.length,
-        currentImage: imagesPreload[prevState.currentImageCounter],
-      }));
+  useEffect(() => {
+    const imageChange = setInterval(() => {
+      setCurrentImage(current =>
+        current === imagesPreload.length - 1 ? 0 : current + 1,
+      );
     }, 5000);
-  }
 
-  render() {
-    const { currentImage } = this.state;
-    return (
-      <article
-        style={{
-          backgroundImage: `url(${currentImage})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <HeadArticleSearch />
-      </article>
-    );
-  }
-}
+    return () => {
+      clearInterval(imageChange);
+    };
+  }, []);
+
+  return (
+    <article className="banner">
+      <LazyLoadImage
+        alt="background"
+        effect="blur"
+        src={imagesPreload[currentImage]}
+      />
+      <div className="banner-text">
+        <h3>Don’t stay at home! </h3>
+        <h4>Join Us To Have Fun And Improve Yourself</h4>
+      </div>
+      <Link to="/home/events" className="banner-button" type="button">
+        Look For Events
+      </Link>
+    </article>
+  );
+};
 
 export default HeadArticle;
