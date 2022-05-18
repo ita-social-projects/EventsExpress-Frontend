d95c0def2b11ef3c007687e76743d8f0ba0abbd7
sasha_popovych <sashapopovych2003@gmail.com>
2022-05-18 09:17:35 +0300

    make a header for laptop monitor

---
+++ src/components/Header/HeaderLinks/HeaderLinks.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  ABOUT_US,
+  CREATE,
+  ENG,
+  EVENTS,
+  LOGIN,
+} from "../../../constants/Header";
+import union from "./icons/union.png";
+import planet from "./icons/planet.png";
+import "./HeaderLinks.scss";
+
+const HeaderLinks = () => {
+  return (
+    <nav>
+      <ul className="header-links-wrapper">
+        <li>
+          <Link className="link-wrapper" to="/about">{`${ABOUT_US}`}</Link>
+        </li>
+        <li>
+          <Link className="link-wrapper" to="/home/events">{`${EVENTS}`}</Link>
+        </li>
+        <li>
+          <Link className="link-wrapper" to="/#">{`${CREATE}`}</Link>
+        </li>
+
+        <li>
+          <Link className="login-link-wrapper" to="/#">
+            <div>{`${LOGIN}`}</div>
+            <img src={union} alt="" />
+          </Link>
+        </li>
+
+        <li>
+          <Link className="eng-link-wrapper" to="/#">
+            <div to="/#">{`${ENG}`}</div>
+            <img src={planet} alt="" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HeaderLinks;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Logo from "../shared/Logo/logo";
-import HeaderRightBlock from "./headerRightBlock/headerRightBlock";
-import MenuIcon from "../../assets/icons/header/Menu.png";
 import "./header.scss";
+import HeaderLinks from "./HeaderLinks/HeaderLinks";
 
 const Header = () => {
   return (
     <nav className="header">
-      <button type="button" tabIndex={0} className="header__menu">
-        <img src={MenuIcon} alt="Menu" />
-      </button>
       <Logo />
-      <HeaderRightBlock />
+      <HeaderLinks />
     </nav>
   );
 };
