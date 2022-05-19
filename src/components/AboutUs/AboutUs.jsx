c79ec9ca043ec191f9ba2f364845115f884abe27
Melya <melenko55@gmail.com>
2022-05-19 16:36:10 +0300

    Landing desktop

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,65 +1,82 @@
 import React from "react";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import aboutImg1 from "../../assets/images/aboutUs/aboutImg-1.png";
 import aboutImg2 from "../../assets/images/aboutUs/aboutImg-2.png";
 import aboutImg3 from "../../assets/images/aboutUs/aboutImg-3.png";
 import aboutImg4 from "../../assets/images/aboutUs/aboutImg-4.png";
-import aboutImg5 from "../../assets/images/aboutUs/aboutImg-5.png";
 import constants from "../../constants/AboutUs";
-import ModalWind from "../modal-wind";
 import "./AboutUs.scss";
 
 const {
-  JOIN_US,
-  ABOUT_US,
-  CONTACT_US,
-  SEARCH_EVENTS,
-  CREATE_YOUR_EVENT,
-  FIND_EVENTS,
+  BANNER_QUOTE,
+  EVENTS_BTN_TEXT,
+  TEXT_BLOCK1_HEADING,
+  TEXT_BLOCK1_BODY,
   GET_CONNECTED,
+  CREATE_YOUR_EVENT,
   HAVE_FUN_TOGETHER,
-  TEXT_INFO,
+  NEW_CONTACTS,
+  TEXT_BLOCK2_HEADING,
+  TEXT_BLOCK2_BODY,
+  MEMBER1,
+  MEMBER2,
+  MEMBER3,
 } = constants;
 
-const MAPPER = [
-  { img: aboutImg1, text: SEARCH_EVENTS },
+const INFO_CARDS = [
+  { img: aboutImg1, text: GET_CONNECTED },
   { img: aboutImg2, text: CREATE_YOUR_EVENT },
-  { img: aboutImg3, text: FIND_EVENTS },
-  { img: aboutImg4, text: GET_CONNECTED },
-  { img: aboutImg5, text: HAVE_FUN_TOGETHER },
+  { img: aboutImg3, text: HAVE_FUN_TOGETHER },
+  { img: aboutImg4, text: NEW_CONTACTS },
 ];
 
-const AboutUs = ({ user }) => {
+const MEMBERS = [MEMBER1, MEMBER2, MEMBER3];
+
+const AboutUs = () => {
   return (
-    <div className="container about__container">
-      <div className="about__info">
-        <h1 className="about__header"> {ABOUT_US} </h1>
-        <div className="about__info_text">{TEXT_INFO}</div>
-        {user.id === null ? (
-          <ModalWind
-            renderButton={action => (
-              <button
-                className="about__info__button"
-                onClick={action}
-                type="button"
-              >
-                {JOIN_US}
-              </button>
-            )}
-          />
-        ) : (
-          <NavLink to="/contactAdmin" className="about__info__button">
-            {CONTACT_US}
+    <div className="about">
+      <div className="about-banner">
+        <div className="banner-content">
+          <h1 className="banner-content__quote">{BANNER_QUOTE}</h1>
+          <NavLink to="/home/events" className="btn-events">
+            {EVENTS_BTN_TEXT}
           </NavLink>
-        )}
+        </div>
       </div>
-      <div className="about__photos">
-        {MAPPER.map(({ img, text }) => (
-          <div key={text} className="about__photos_item">
-            <img src={img} alt={text} />
-            <div className="about__photos_text">{text}</div>
+
+      <div className="about-info">
+        <div className="info-text__left">
+          <h1 className="info-text__header">{TEXT_BLOCK1_HEADING}</h1>
+          <p className="info-text__body">{TEXT_BLOCK1_BODY}</p>
+        </div>
+        <div className="info-images">
+          {INFO_CARDS.map(({ img, text }) => (
+            <div key={text} className="info-images__item">
+              <img src={img} alt={text} />
+              <div className="info-images__text">{text}</div>
+            </div>
+          ))}
+        </div>
+        <div className="right-container">
+          <div className="info-text__right">
+            <h1 className="info-text__header">{TEXT_BLOCK2_HEADING}</h1>
+            <p className="info-text__body">{TEXT_BLOCK2_BODY}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-members">
+        {MEMBERS.map(({ name, description, img }) => (
+          <div className="member" key={name}>
+            <div className="member__image">
+              <img src={img} alt={name} />
+            </div>
+            <div className="member__info">
+              <h1 className="member__name">{name}</h1>
+              <p className="member__description">{description}</p>
+            </div>
           </div>
         ))}
       </div>
@@ -67,13 +84,13 @@ const AboutUs = ({ user }) => {
   );
 };
 
-AboutUs.defaultProps = {
-  user: {},
-};
+// AboutUs.defaultProps = {
+//   user: {},
+// };
 
-AboutUs.propTypes = {
-  user: PropTypes.object,
-};
+// AboutUs.propTypes = {
+//   user: PropTypes.object,
+// };
 
 const mapStateToProps = state => {
   return {
--- src/constants/AboutUs.js
+++ src/constants/AboutUs.js
@@ -1,14 +1,34 @@
 const ABOUT_US = {
-  JOIN_US: "Join us",
-  ABOUT_US: "About us",
-  CONTACT_US: "Contact us",
-  SEARCH_EVENTS: "Search events",
-  CREATE_YOUR_EVENT: "Create your event",
-  FIND_EVENTS: "Find events",
-  GET_CONNECTED: "Get connected",
-  HAVE_FUN_TOGETHER: "Have fun together",
-  TEXT_INFO:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum. ",
+  BANNER_QUOTE: "Exclusive events, priceless memories.",
+  EVENTS_BTN_TEXT: "Look for events",
+  TEXT_BLOCK1_HEADING: "About Us",
+  TEXT_BLOCK1_BODY:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  GET_CONNECTED: "Get Connected",
+  CREATE_YOUR_EVENT: "Create Your Event",
+  HAVE_FUN_TOGETHER: "Have Fun Together",
+  NEW_CONTACTS: "New Contacts",
+  TEXT_BLOCK2_HEADING: "With Love",
+  TEXT_BLOCK2_BODY:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.s",
+  MEMBER1: {
+    name: "Ellen, 45",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis elementum elit cursus et mattis ac. Ullamcorper sed egestas sed nunc proin vel cursus odio dolor. Senectus tincidunt enim laoreet suspendisse ",
+    img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880",
+  },
+  MEMBER2: {
+    name: "Sasha, 19",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis elementum elit cursus et mattis ac. Ullamcorper sed egestas sed nunc proin vel cursus odio dolor. Senectus tincidunt enim laoreet suspendisse s",
+    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+  },
+  MEMBER3: {
+    name: "Steve, 32",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis elementum elit cursus et mattis ac. Ullamcorper sed egestas sed nunc proin vel cursus odio dolor. Senectus tincidunt enim laoreet suspendisse fh",
+    img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880",
+  },
 };
 
 export default ABOUT_US;
