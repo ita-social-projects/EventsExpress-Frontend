235eeb2b74c5ecb06a2e2bfcd3a6c75459016855
Melya <melenko55@gmail.com>
2022-05-28 15:40:23 +0300

    Styles constants fixup

--- src/components/landing/HeadArticle/HeadArticle.js
+++ src/components/landing/HeadArticle/HeadArticle.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-
 import image1 from "../landing-images/1.jpg";
 import image2 from "../landing-images/2.jpg";
 import image3 from "../landing-images/3.jpg";
@@ -10,14 +9,11 @@ import image5 from "../landing-images/5.jpg";
 import image6 from "../landing-images/6.jpg";
 import image7 from "../landing-images/7.jpg";
 import image8 from "../landing-images/8.jpg";
-
 import "./HeadArticle.scss";
 import "react-lazy-load-image-component/src/effects/blur.css";
-
 import constants from "../../../constants/HeadArticle";
 
 const { BANNER_TEXT_1, BANNER_TEXT_2, BANNER_BUTTON_TEXT } = constants;
-
 const imagesPreload = [
   image1,
   image2,
@@ -48,8 +44,8 @@ const HeadArticle = () => {
     <article className="banner">
       <div className="banner-content">
         <div className="banner-text">
-          <h3>{BANNER_TEXT_1}</h3>
-          <h4>{BANNER_TEXT_2}</h4>
+          <h3 className="banner-text__quote1">{BANNER_TEXT_1}</h3>
+          <h4 className="banner-text__quote2">{BANNER_TEXT_2}</h4>
         </div>
         <Link to="/home/events" className="banner-button" type="button">
           {BANNER_BUTTON_TEXT}
