9c47a8a64e66c79c18d621f9bb239cfe857d33c4
Melya <melenko55@gmail.com>
2022-05-30 10:53:49 +0300

    create landing slideshow constants file and fix styles

--- src/components/landing/HeadArticle/HeadArticle.js
+++ src/components/landing/HeadArticle/HeadArticle.js
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import image1 from "../landing-images/1.jpg";
-import image2 from "../landing-images/2.jpg";
-import image3 from "../landing-images/3.jpg";
-import image4 from "../landing-images/4.jpg";
-import image5 from "../landing-images/5.jpg";
-import image6 from "../landing-images/6.jpg";
-import image7 from "../landing-images/7.jpg";
-import image8 from "../landing-images/8.jpg";
 import "./HeadArticle.scss";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import constants from "../../../constants/HeadArticle";
+import slideShowimages, { SLIDES_INTERVAL } from "./HeadArticleSlideShow";
 
 const { BANNER_TEXT_1, BANNER_TEXT_2, BANNER_BUTTON_TEXT } = constants;
-const imagesPreload = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-];
 
 const HeadArticle = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -31,9 +14,9 @@ const HeadArticle = () => {
   useEffect(() => {
     const imageChange = setInterval(() => {
       setCurrentImage(current =>
-        current === imagesPreload.length - 1 ? 0 : current + 1,
+        current === slideShowimages.length - 1 ? 0 : current + 1,
       );
-    }, 5000);
+    }, SLIDES_INTERVAL);
 
     return () => {
       clearInterval(imageChange);
@@ -55,7 +38,7 @@ const HeadArticle = () => {
         <LazyLoadImage
           alt="background"
           effect="blur"
-          src={imagesPreload[currentImage]}
+          src={slideShowimages[currentImage]}
           wrapperClassName="banner-img__wrapper"
         />
       </div>
