51d90b92e5679a95356c8fbedf535a6f34195e4d
na3alis <na3alis@gmail.com>
2022-05-31 08:56:58 +0300

    EV-25 re-mockup-and-add-photo-to-events

--- src/components/landing/EventCard/EventCard.js
+++ src/components/landing/EventCard/EventCard.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import { BiRightArrowAlt } from "react-icons/bi";
 import moment from "moment";
 import IconsEventCard from "./IconsEventCard/IconsEventCard";
 import "./EventCard.scss";
+import PhotoService from "../../../services/PhotoService";
+import eventDefaultImage from "../../../constants/eventDefaultImage";
 
 const EventCard = ({ event }) => {
-  const { id, title, photo, dateFrom } = event;
+  const { id, title, dateFrom } = event;
   const monthes = [
     "January",
     "February",
@@ -23,21 +25,35 @@ const EventCard = ({ event }) => {
     "December",
   ];
   const monthIndex = moment(dateFrom).format("M") - 1;
+  const photoService = new PhotoService();
+  const [eventImage, setEventImage] = useState(eventDefaultImage);
+
+  useEffect(() => {
+    photoService.getPreviewEventPhoto(id).then(eventPreviewImage => {
+      if (eventPreviewImage) {
+        const image = URL.createObjectURL(eventPreviewImage);
+        setEventImage(image);
+      }
+    });
+    return () => {
+      URL.revokeObjectURL(eventImage);
+    };
+  }, []);
 
   return (
     <div className="event-card">
-      <div className="card-image-container">
-        <img className="card-image" src={photo} alt="" />
-      </div>
+      <img
+        className="card-image-container card-image"
+        src={eventImage}
+        alt=""
+      />
       <div className="card-content">
-        <div className="card-date">
-          <div className="date-container">
-            <span className="day">{moment(dateFrom).format("DD")}</span>
-          </div>
+        <div className="card-date date-container">
+          <span className="day">{moment(dateFrom).format("DD")}</span>
         </div>
         <div className="card-info">
           <span className="card-info-header">
-            {title.length > 55 ? `${title.slice(0, 55)}...` : title}
+            {title.length > 35 ? `${title.slice(0, 35)}...` : title}
           </span>
           <div className="card-buttons">
             <IconsEventCard className="like" styleForIcon="list" />
--- src/components/landing/EventsListItem/EventListCard/EventListCard.js
+++ src/components/landing/EventsListItem/EventListCard/EventListCard.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import { BiRightArrowAlt } from "react-icons/bi";
 import moment from "moment";
 import IconsEventCard from "../../EventCard/IconsEventCard/IconsEventCard";
 import "./EventListCard.scss";
+import PhotoService from "../../../../services/PhotoService";
+import eventDefaultImage from "../../../../constants/eventDefaultImage";
 
 const EventListCard = ({ event }) => {
-  const { id, photo, title, description, location, dateFrom } = event;
+  const { id, title, description, location, dateFrom } = event;
   const ovner = event.organizers[0].username;
   const monthes = [
     "January",
@@ -24,34 +26,50 @@ const EventListCard = ({ event }) => {
     "December",
   ];
   const monthIndex = moment(dateFrom).format("M") - 1;
+  const photoService = new PhotoService();
+  const [eventImage, setEventImage] = useState(eventDefaultImage);
+
+  useEffect(() => {
+    photoService.getPreviewEventPhoto(id).then(eventPreviewImage => {
+      if (eventPreviewImage) {
+        const image = URL.createObjectURL(eventPreviewImage);
+        setEventImage(image);
+      }
+    });
+    return () => {
+      URL.revokeObjectURL(eventImage);
+    };
+  }, []);
 
   return (
-    <div className="card__item_container">
-      <div className="card__item">
+    <div className="card-item-container">
+      <div className="card-item">
         <div className="content">
           <div className="image-container">
-            <img className="card-image" src={photo} alt="Event_photo" />
+            <img className="card-image" src={eventImage} alt="" />
           </div>
-          <div className="card__item_content">
-            <h2 className="card__item_header">
+          <div className="card-item-content">
+            <h2 className="card-item-header">
               {title.length > 35 ? `${title.slice(0, 35)}...` : title}
             </h2>
-            <p className="card__item-ovner">{`By ${ovner}`}</p>
-            <p className="card__item_description">
+            <p className="card-item-ovner">{`By ${ovner}`}</p>
+            <p className="card-item-description">
               {`${description.slice(0, 100)}...`}
             </p>
           </div>
         </div>
-        <div className="card__item_bottom-row">
+        <div className="card-item-bottom-row">
           <div className="event-date">
             <span className="day">{moment(dateFrom).format("DD")}</span>
             <span className="month">{monthes[monthIndex]}</span>
           </div>
-          <span className="card__item_footer event-location">{location}</span>
-          <IconsEventCard className="card__item_footer" styleForIcon="list" />
+          <span className="card-item-footer event-location">
+            {location.latitude ? "Map" : "Online"}
+          </span>
+          <IconsEventCard className="card-item-footer" styleForIcon="list" />
           <NavLink
             to={`/home/events/${id}`}
-            className="card__item_footer event-details-btn bttn"
+            className="card-item-footer event-details-btn bttn"
           >
             <BiRightArrowAlt size={30} />
           </NavLink>
