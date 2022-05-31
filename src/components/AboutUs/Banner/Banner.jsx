93b644304eaa526e54d7e5bb964d278af135cb78
Melya <melenko55@gmail.com>
2022-05-31 10:33:29 +0300

    remove returns

--- src/components/AboutUs/Banner/Banner.jsx
+++ src/components/AboutUs/Banner/Banner.jsx
@@ -3,15 +3,13 @@ import { NavLink } from "react-router-dom";
 import { BANNER_QUOTE, EVENTS_BTN_TEXT } from "../../../constants/AboutUs";
 import "./Banner.scss";
 
-const Banner = () => {
-  return (
-    <div className="about-banner">
-      <h1 className="banner-content__quote">{BANNER_QUOTE}</h1>
-      <NavLink to="/home/events" className="btn-events">
-        {EVENTS_BTN_TEXT}
-      </NavLink>
-    </div>
-  );
-};
+const Banner = () => (
+  <div className="about-banner">
+    <h1 className="banner-content__quote">{BANNER_QUOTE}</h1>
+    <NavLink to="/home/events" className="btn-events">
+      {EVENTS_BTN_TEXT}
+    </NavLink>
+  </div>
+);
 
 export default Banner;
