ec88105128e27ed9dbb750eb50d8c46dc9539467
Danyil <demyanyuk76@gmail.com>
2022-06-02 11:05:56 +0300

    fix buttonNamesConstants

--- src/constants/ButtonNamesConstants.js
+++ src/constants/ButtonNamesConstants.js
@@ -1,6 +1,6 @@
-const ButtonNames = {
+const BUTTON_NAMES = {
   GOOGLE_LOGIN: "Log in with Google",
   FACEBOOK_LOGIN: "Log in with Facebook",
 };
 
-export default ButtonNames;
+export default BUTTON_NAMES;
