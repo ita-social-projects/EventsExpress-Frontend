4f1796bbe2b7d6a61068f2d7e38700af32500f41
sasha_popovych <sashapopovych2003@gmail.com>
2022-06-07 19:14:43 +0300

    remove index file from app folder

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-import "./app.css";
+import "./app.scss";
 
 import { connect } from "react-redux";
 import propTypes from "prop-types";
