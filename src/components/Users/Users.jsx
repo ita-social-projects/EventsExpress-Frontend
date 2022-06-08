14c7adffe193c7bcf3a15e26a855fc5861bc030b
Nazariy Stodola <onepiece.nazar@gmail.com>
2022-06-08 22:22:50 +0300

    fix last imports

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Users.scss";
 import UserInfoWrapper from "../../containers/user-info";
 import PagePagination from "../shared/PagePagination/PagePagination";
+import "./Users.scss";
 
 const Users = ({ page, totalPages, users, callback }) => {
   const handlePageChange = pageEl => {
