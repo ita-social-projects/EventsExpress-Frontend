37e440a4acaa75857024b039f0bcbbba6389199e
Melya <melenko55@gmail.com>
2022-06-14 09:56:07 +0300

    Rewrite o functional

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,5 @@
 import "./RegistrationForm.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Stepper from "../stepper/Stepper";
 import CompleteProfileForm from "./CompleteProfileForm";
 import ConfirmForm from "./ConfirmForm";
@@ -20,66 +20,48 @@ const {
   STEP_CONFIRM,
   STEP_SUCCESS,
 } = steps;
-export default class RegistrationForm extends Component {
-  constructor(props) {
-    super(props);
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
-    this.state = {
-      currentStep: 2,
-    };
-  }
 
-  nextPage() {
-    this.setState(prevState => ({ currentStep: prevState.currentStep + 1 }));
-  }
+const RegistrationForm = () => {
+  const [currentStep, setCurrentStep] = useState(2);
+  const nextPage = () => {
+    setCurrentStep(currentStep + 1);
+  };
+  const previousPage = () => {
+    setCurrentStep(currentStep - 1);
+  };
 
-  previousPage() {
-    this.setState(prevState => ({ currentStep: prevState.currentStep - 1 }));
-  }
-
-  render() {
-    // TODO: don`t use this props (it`s func)
-    // const { onSubmit } = this.props;
-    const { currentStep } = this.state;
-
-    return (
-      <>
-        <div className="stepper-container-horizontal">
-          <Stepper
-            currentStepNumber={currentStep - 1}
-            steps={[REGISTER, COMPLETE, STEP_3, STEP_4, CONFIRM]}
-            stepColor="#ff9900"
-          />
-          <br />
-          <div className="buttons-container">
-            <div>
-              {currentStep === STEP_COMPLETE && (
-                <CompleteProfileForm onSubmit={this.nextPage} />
-              )}
-              {currentStep === STEP_CHOOSE_ACTIVITIES && (
-                <ChooseActivities
-                  previousPage={this.previousPage}
-                  onSubmit={this.nextPage}
-                />
-              )}
-              {currentStep === STEP_PLACE_HOLDER && (
-                <PlaceHolder
-                  previousPage={this.previousPage}
-                  onSubmit={this.nextPage}
-                />
-              )}
-              {currentStep === STEP_CONFIRM && (
-                <ConfirmForm
-                  previousPage={this.previousPage}
-                  onSubmit={this.nextPage}
-                />
-              )}
-              {currentStep === STEP_SUCCESS && <SuccessResult />}
-            </div>
+  return (
+    <>
+      <div className="stepper-container-horizontal">
+        <Stepper
+          currentStepNumber={currentStep - 1}
+          steps={[REGISTER, COMPLETE, STEP_3, STEP_4, CONFIRM]}
+          stepColor="#ff9900"
+        />
+        <br />
+        <div className="buttons-container">
+          <div>
+            {currentStep === STEP_COMPLETE && (
+              <CompleteProfileForm onSubmit={nextPage} />
+            )}
+            {currentStep === STEP_CHOOSE_ACTIVITIES && (
+              <ChooseActivities
+                previousPage={previousPage}
+                onSubmit={nextPage}
+              />
+            )}
+            {currentStep === STEP_PLACE_HOLDER && (
+              <PlaceHolder previousPage={previousPage} onSubmit={nextPage} />
+            )}
+            {currentStep === STEP_CONFIRM && (
+              <ConfirmForm previousPage={previousPage} onSubmit={nextPage} />
+            )}
+            {currentStep === STEP_SUCCESS && <SuccessResult />}
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
+
+export default RegistrationForm;
