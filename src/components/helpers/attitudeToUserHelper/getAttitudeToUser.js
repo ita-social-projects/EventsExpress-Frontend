c7bfc60d52fa5ac5efda81a8785748f756e236e8
Melya <melenko55@gmail.com>
2022-06-18 15:14:26 +0300

    Recover old version of getAttitude

--- src/components/helpers/attitudeToUserHelper/getAttitudeToUser.js
+++ src/components/helpers/attitudeToUserHelper/getAttitudeToUser.js
@@ -4,26 +4,28 @@ import {
 } from "../../../constants/UserAttitudeConstants";
 
 const getAttitudeToUser = attitude => {
-  // Like = 1, Dislike = -1, Neutral = 0
-  if (attitude === 0) {
-    return {
-      bg: "attitude_bg_green",
-      message: ATTITUDE_MESSAGES.LIKE,
-      thumb: ATTITUDE_ICONS.THUMB_UP,
-    };
-  }
+  // Like = 0, Dislike = 1
+  let attitudeToUser = {};
 
-  return attitude === 1
-    ? {
+  switch (attitude) {
+    case 0:
+      attitudeToUser = {
+        bg: "attitude_bg_green",
+        message: ATTITUDE_MESSAGES.LIKE,
+        thumb: ATTITUDE_ICONS.THUMB_UP,
+      };
+      break;
+    case 1:
+      attitudeToUser = {
         bg: "attitude_bg_red",
         message: ATTITUDE_MESSAGES.DISLIKE,
         thumb: ATTITUDE_ICONS.THUMB_DOWN,
-      }
-    : {
-        bg: null,
-        message: null,
-        thumb: null,
       };
+      break;
+    default:
+      attitudeToUser = { bg: null, message: null, thumb: null };
+  }
+  return attitudeToUser;
 };
 
 export default getAttitudeToUser;
