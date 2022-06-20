d03ab9a4281869e29f8a94e1dad0d132de3e3933
Danyil <demyanyuk76@gmail.com>
2022-06-20 23:48:09 +0300

    fix errors

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./app.scss";
-
 import { connect } from "react-redux";
 import propTypes from "prop-types";
 import LandingContainer from "../../containers/LandingContainer/LandingContainer";
